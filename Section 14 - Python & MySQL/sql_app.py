76108a6d65533e6aeb05541449d5ffbccf8eb581
Ahmad Zafar <62062766+TheTurtle3@users.noreply.github.com>
2021-05-18 11:11:12 -0400

    Create sql_app.py

---
+++ Section 14 - Python & MySQL/sql_app.py
@@ -0,0 +1,8 @@
+import mysql.connector
+
+con = mysql.connector.connect(
+    user = "ardit700_student",
+    password = "ardit7000_student",
+    host = "108.67.140.122",
+    database = "ardit700_pm1database"
+)
