f23158d54e451cc6e414a95fa0ef0802da74535e
Ahmad Zafar <amz9616@gmail.com>
2021-07-22 16:17:12 -0400

    Update sql_app.py

--- Section 14 - Python & MySQL/sql_app.py
+++ Section 14 - Python & MySQL/sql_app.py
@@ -1,3 +1,4 @@
+from re import T
 import mysql.connector
 
 # connects to ardit's dictionary database
@@ -8,9 +9,27 @@ con = mysql.connector.connect(
     database = "ardit700_pm1database"
 )
 
-cursor = con.cursor()
+def func(user_word):
+    cursor = con.cursor()
 
-query = cursor.execute("SELECT * FROM Dictionary WHERE Expression = 'line' ")
-results = cursor.fetchall()
+    # a table to store userinput for SQL query
+    # cursor.execute("CREATE TABLE DemoTable ")
 
-print(results)
+    # givenUsername = 'testUser123'
+    # checkUserAuthQuery = ("SELECT password FROM UserAuth WHERE username = %s")
+    # userAuthInfo = (givenUsername)
+    # cursor.execute(checkUserAuthQuery, userAuthInfo)
+
+    query = cursor.execute("SELECT * FROM Dictionary WHERE Expression = '{}'".format(user_word))
+    results = cursor.fetchall()
+    
+    return results
+
+while True:
+    user_input = input("Enter a word: ")
+    output = func(user_input)
+    
+    print(output)
+
+    if(input("Do you want to continue? (Y/N) ").lower() == "n"):
+        break
